45c7ccfb58f8f9e0327ccce7f9e5e4f6455dcdde
Israel-Yarbloom <i0504170922@gmail.com>
2026-02-05 13:13:58 +0200

    finish

--- app/routes.py
+++ app/routes.py
@@ -22,19 +22,19 @@ def employees_by_age_and_role():
 
 @router.get("/employees/top-seniority")
 def top_seniority_employees_excluding():
-    return get_top_seniority_employees_excluding_hr
+    return get_top_seniority_employees_excluding_hr()
 
 
 @router.get("/employees/age-or-seniority")
 def employees_by_age_or_seniority():
-    return get_employees_by_age_or_seniority
+    return get_employees_by_age_or_seniority()
 
 
 @router.get("/employees/managers/excluding-departments")
 def managers_excluding_departments():
-    return get_managers_excluding_departments
+    return get_managers_excluding_departments()
 
 
 @router.get("/employees/by-lastname-and-age")
 def employees_by_lastname_and_age():
-    return get_employees_by_lastname_and_age
+    return get_employees_by_lastname_and_age()
